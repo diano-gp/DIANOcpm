1f1bf0bd40655f64864908e19a7a2d2ef80a46f6
Anozie <anozie804@gmail.com>
2025-03-08 16:08:46 +0100

    Update hadjimedia.py

--- hadjimedia.py
+++ hadjimedia.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://godgs.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://ayandev.squareweb.app/api"
 
 class HadjiMedia:
     def __init__(self, access_key) -> None:
